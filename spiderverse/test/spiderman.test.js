4b7e04f40c111da0cfb151b5513cf3b1cba0d7ca
skrillheaven <beto.jacp2010@gmail.com>
2022-07-05 17:27:13 -0500

    Step 1: Write a test for the next bit of functionality you want to add

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -1,8 +1,24 @@
-describe("Test Suite Dummy Description", () => {
+/*describe("Test Suite Dummy Description", () => {
     test('Case 1 Dummy', () => {
         const resultOfSomething = 1 + 2 //resultado de algo
         expect(resultOfSomething).toBe(18);
     })
 })
 
-//Test Driven Development by Martin Fowler
+//Test Driven Development by Martin Fowler*/
+
+
+describe("Unit Test for Spiderman Class", () => {
+    test('1) Create a spiderman object ', () => {
+        const andrewGarfield = new Spiderman(
+                "spiderman Sony", 31, "Andrew Garfield", 2, "The amazin spiderman", "Sony"
+            )
+            //validamos que elk codigo funcione
+
+        expect(andrewGarfield.name).toBe("spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.studio).toBe("Sony")
+    });
+})
