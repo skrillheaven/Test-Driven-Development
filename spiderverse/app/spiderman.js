6bc94ce1bce8931c8e44b970fd378c41ce211272
skrillheaven <beto.jacp2010@gmail.com>
2022-07-06 14:50:33 -0500

    Agregando constructores necesarios para la prueba

--- spiderverse/app/spiderman.js
+++ spiderverse/app/spiderman.js
@@ -1,6 +1,11 @@
 class Spiderman {
-    constructor(name) { this.name = name }
-
+    constructor(name, age, actor, movies, studio) {
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.movies = movies
+        this.studio = studio
+    }
 }
 
 module.exports = Spiderman
