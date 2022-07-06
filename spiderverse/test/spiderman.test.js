f84971dae0b62a17643b944b353fcad03a7e81e1
skrillheaven <beto.jacp2010@gmail.com>
2022-07-06 14:55:47 -0500

    St↓↓: Write the functional code until the test passes.↓

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -7,18 +7,18 @@
 
 //Test Driven Development by Martin Fowler*/
 
-
+const Spiderman = require('./../app/spiderman')
 describe("Unit Test for Spiderman Class", () => {
     test('1) Create a spiderman object ', () => {
         const andrewGarfield = new Spiderman(
                 "spiderman Sony", 31, "Andrew Garfield", 2, "The amazin spiderman", "Sony"
             )
-            //validamos que elk codigo funcione
+            //validamos que el codigo funcione
 
         expect(andrewGarfield.name).toBe("spiderman Sony")
         expect(andrewGarfield.age).toBe(31)
         expect(andrewGarfield.actor).toBe("Andrew Garfield")
-        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
     });
 })
